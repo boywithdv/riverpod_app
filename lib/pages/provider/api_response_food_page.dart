9e8649bd8f754cb873bfdba0f8f81ab8187d272f
tsukuda <jukiya8891@gmail.com>
2024-09-30 08:48:38 +0900

    feat: 画像が読み込めない場合には再読み込みを行えるようにする

--- lib/pages/provider/api_response_food_page.dart
+++ lib/pages/provider/api_response_food_page.dart
@@ -4,9 +4,31 @@ import 'package:gap/gap.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_app/features/food_provider.dart';
 
-class ApiResponseFoodPage extends ConsumerWidget {
+class ApiResponseFoodPage extends ConsumerStatefulWidget {
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  _ApiResponseFoodPageState createState() => _ApiResponseFoodPageState();
+}
+
+class _ApiResponseFoodPageState extends ConsumerState<ApiResponseFoodPage> {
+  // 再読み込み用のキー
+  late int _reloadKey;
+  late String _imageUrl = 'https://loremfdfdfdlickr.com/g/320/240/paris,girl/all';
+
+  @override
+  void initState() {
+    super.initState();
+    _reloadKey = 0; // 初期化
+  }
+
+  void _reloadImage() {
+    setState(() {
+      _imageUrl = 'https://loremflickr.com/g/320/240/paris,girl/all';
+      _reloadKey++; // 状態を変更して再読み込みをトリガー
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final foodAsyncValue = ref.watch(foodsProvider());
 
     return Scaffold(
@@ -29,7 +51,37 @@ class ApiResponseFoodPage extends ConsumerWidget {
                     ),
                     const Gap(8),
                     Text(food.image?.first.id.toString() ?? 'dd'),
-                    CachedNetworkImage(imageUrl:'https://loremflickr.com/g/320/240/paris,girl/all'),
+                    CachedNetworkImage(
+                      key: ValueKey(_reloadKey), // キーを変更して再レンダリングを促す
+                      imageUrl: _imageUrl,
+                      imageBuilder: (context, imageProvider) {
+                        return Container(
+                          height: 200,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: imageProvider,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        );
+                      },
+                      placeholder: (context, url) {
+                        return CircularProgressIndicator();
+                      },
+                      errorWidget: (context, url, error) {
+                        return Column(
+                          children: [
+                            Icon(Icons.error),
+                            Text('Failed to load image.'),
+                            ElevatedButton(
+                              // 画像の再読み込みを行う
+                              onPressed: _reloadImage, 
+                              child: Text('Retry'),
+                            ),
+                          ],
+                        );
+                      },
+                    ),
                     const Divider(),
                   ],
                 );
