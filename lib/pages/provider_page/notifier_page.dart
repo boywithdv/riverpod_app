32e515327e2319328b47762cd5766ac879c21139
佃寿希也 <tsukudajukiya@team-lab.com>
2025-05-11 22:16:35 +0900

    fix: invalidateを追加する

--- lib/pages/provider_page/notifier_page.dart
+++ lib/pages/provider_page/notifier_page.dart
@@ -5,72 +5,104 @@ import 'package:riverpod_app/features/int_notifier_provider.dart';
 
 import '../../features/list_notifier_provider.dart';
 
-
 class NotifierPage extends ConsumerWidget {
-  
   const NotifierPage({super.key});
 
   @override
-  Widget build(BuildContext context,WidgetRef ref) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final s1Notifier = ref.watch(s1NotifierProvider);
     final s2Notifier = ref.watch(s2NotifierProvider);
     final s3Notifier = ref.watch(s3NotifierProvider);
-    final widget = s3Notifier.when(data:(d)=> d , error:(e,_)=> Icon(Icons.error), loading:()=> '接続中');
+    final widget = s3Notifier.when(
+        data: (d) => d,
+        error: (e, _) => Icon(Icons.error),
+        loading: () => '接続中');
 
-    ref.listen(
-      s1NotifierProvider,(oldState,newState){
-        // スナックバーを表示する
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('${oldState}から $newState に変更されました。')));
-      }
-    );
-    ref.listen(
-      s2NotifierProvider,(previousState,newState){
-        // スナックバーを表示する
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('$previousState から $newState に変更されました。')));
-      }
-    );
-    ref.listen(
-      s3NotifierProvider,(previousState,newState){
-        // スナックバーを表示する
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('$previousState から $newState に変更されました。')));
-      } 
-    );
+    ref.listen(s1NotifierProvider, (oldState, newState) {
+      // スナックバーを表示する
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('${oldState}から $newState に変更されました。')));
+    });
+    ref.listen(s2NotifierProvider, (previousState, newState) {
+      // スナックバーを表示する
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('$previousState から $newState に変更されました。')));
+    });
+    ref.listen(s3NotifierProvider, (previousState, newState) {
+      // スナックバーを表示する
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('$previousState から $newState に変更されました。')));
+    });
     return Scaffold(
       appBar: AppBar(
         title: Text('NotifierPage'),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Text('Increment :$s1Notifier'),
-              IconButton(onPressed: (){
-                ref.read(s1NotifierProvider.notifier).increment();
-              }, icon: Icon(Icons.add))
-              
-              
+              IconButton(
+                onPressed: () {
+                  ref.read(s1NotifierProvider.notifier).increment();
+                },
+                icon: Icon(Icons.add),
+              )
+            ],
+          ),
+          InvalidateButton(onTap: () {
+            debugPrint('${s1Notifier} が初期の状態に戻りました。');
+            ref.invalidate(s1NotifierProvider);
+          }),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('List :$s2Notifier'),
+              IconButton(
+                  onPressed: () {
+                    ref.read(s2NotifierProvider.notifier).add();
+                  },
+                  icon: Icon(Icons.add))
             ],
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text('List :$s2Notifier'),
-              IconButton(onPressed: (){
-                ref.read(s2NotifierProvider.notifier).add();
-              }, icon: Icon(Icons.add))
-            ],
-          ),
-                    Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
               Text('Future :$widget'),
-              IconButton(onPressed: (){
-                ref.read(s3NotifierProvider.notifier).UpdateState();
-              }, icon: Icon(Icons.add))
+              IconButton(
+                  onPressed: () {
+                    ref.read(s3NotifierProvider.notifier).UpdateState();
+                  },
+                  icon: Icon(Icons.add))
             ],
           ),
         ],
+      ),
+    );
+  }
+}
+
+class InvalidateButton extends StatelessWidget {
+  const InvalidateButton({super.key, required this.onTap});
+
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(255, 110, 239, 253),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      padding: EdgeInsets.all(10),
+      child: InkWell(
+        onTap: onTap,
+        child: const Text(
+          'Invalidate',
+          style: TextStyle(color: Colors.white),
+        ),
       ),
     );
   }
--- lib/pages/provider_page/riverpod_main_page.dart
+++ lib/pages/provider_page/riverpod_main_page.dart
@@ -4,89 +4,193 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import '../../features/features.dart';
 import 'provider_page.dart';
 
-
 class RiverpodMainPage extends ConsumerWidget {
   const RiverpodMainPage({super.key});
 
-
   @override
-  Widget build(BuildContext context,WidgetRef ref) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final appName = ref.watch(appNameProvider);
     final doubleCounter = ref.watch(doubleCounterProvider);
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(appName),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              MaterialButton(onPressed: (){
-                    Navigator.push(context, MaterialPageRoute(builder: (context)=>NotifierPage()));
-                  }, child: Text('Notifier Page',style: TextStyle(color: Colors.blue),),),
-              Consumer(
-                builder: (context, watch, _) {
-                  final myData = ref.watch(myProvider);
-                  return Text(
-                    myData.value,
-                    style: Theme.of(context).textTheme.headlineMedium,
-                  );
-                },
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(appName),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            MaterialButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => NotifierPage()));
+              },
+              child: Text(
+                'Notifier Page',
+                style: TextStyle(color: Colors.blue),
               ),
-              Text('Double Counter Provider Value:$doubleCounter',
-                  style: Theme.of(context).textTheme.headlineMedium),
-              MaterialButton(onPressed: (){
-                    Navigator.push(context, MaterialPageRoute(builder: (context)=>ComsumerWidgetSample()));
-                  }, child: Text('Consumer Widget Sample Page',style: TextStyle(color: Colors.blue),),),
-                            Text('Double Counter Provider Value:$doubleCounter',
-                  style: Theme.of(context).textTheme.headlineMedium),
-              MaterialButton(onPressed: (){
-                    Navigator.push(context, MaterialPageRoute(builder: (context)=>UseRefListenProviderPage()));
-                  }, child: Text('Use ref listen provider Page',style: TextStyle(color: Colors.blue),),),
-              /*
+            ),
+            Consumer(
+              builder: (context, watch, _) {
+                final myData = ref.watch(myProvider);
+                return Text(
+                  myData.value,
+                  style: Theme.of(context).textTheme.headlineMedium,
+                );
+              },
+            ),
+            Text('Double Counter Provider Value:$doubleCounter',
+                style: Theme.of(context).textTheme.headlineMedium),
+            MaterialButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => ComsumerWidgetSample()));
+              },
+              child: Text(
+                'Consumer Widget Sample Page',
+                style: TextStyle(color: Colors.blue),
+              ),
+            ),
+            Text('Double Counter Provider Value:$doubleCounter',
+                style: Theme.of(context).textTheme.headlineMedium),
+            MaterialButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => UseRefListenProviderPage()));
+              },
+              child: Text(
+                'Use ref listen provider Page',
+                style: TextStyle(color: Colors.blue),
+              ),
+            ),
+            /*
               // ref.listenManualを使ったページ
               MaterialButton(onPressed: (){
                     Navigator.push(context, MaterialPageRoute(builder: (context)=>UseRefListenmanualProviderPage()));
                   }, child: Text('Use ref listenManual provider Page',style: TextStyle(color: Colors.blue),),),
               */
-              MaterialButton(onPressed: (){
-                    Navigator.push(context, MaterialPageRoute(builder: (context)=>CounterStateProviderPage()));
-                  }, child: Text('Use ref listenManual provider Page',style: TextStyle(color: Colors.blue),),),
-              // StateNotifierProviderを使ったページ
-              MaterialButton(onPressed: (){
-                    Navigator.push(context, MaterialPageRoute(builder: (context)=>StateNotifierProviderPage()));
-                  }, child: Text('StateNotifierProvider Page',style: TextStyle(color: Colors.blue),),),
-              MaterialButton(onPressed: (){
-                    Navigator.push(context, MaterialPageRoute(builder: (context)=>StreamProviderPage()));
-                  }, child: Text('StreamProvider Page',style: TextStyle(color: Colors.blue),),),
-              MaterialButton(onPressed: (){
-                    Navigator.push(context, MaterialPageRoute(builder: (context)=>ChangeNotifierProviderPage()));
-                  }, child: Text('Change Notifier Provider Page',style: TextStyle(color: Colors.blue),),),
-              MaterialButton(onPressed: (){
-                    Navigator.push(context, MaterialPageRoute(builder: (context)=>ProductDetailPage()));
-                  }, child: Text('Product Detail Page',style: TextStyle(color: Colors.blue),),),
-              MaterialButton(onPressed: (){
-                    Navigator.push(context, MaterialPageRoute(builder: (context)=>ProviderScopePage()));
-                  }, child: Text('Provider Scope Page',style: TextStyle(color: Colors.blue),),),
-              MaterialButton(onPressed: (){
-                    Navigator.push(context, MaterialPageRoute(builder: (context)=>FlutterHooksPage()));
-                  }, child: Text('Flutter Hooks Page',style: TextStyle(color: Colors.blue),),),
-              MaterialButton(onPressed: (){
-                    Navigator.push(context, MaterialPageRoute(builder: (context)=>SharedPreferencesCacheProviderPage()));
-                  }, child: Text('SharedPreferences Cache Provider Page',style: TextStyle(color: Colors.blue),),),
-              MaterialButton(onPressed: (){
-                    Navigator.push(context, MaterialPageRoute(builder: (context)=>ApiResponseFoodPage()));
-                  }, child: Text('Api Response Food Page',style: TextStyle(color: Colors.blue),),),
-            ],
-          ),
+            MaterialButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => CounterStateProviderPage()));
+              },
+              child: Text(
+                'Use ref listenManual provider Page',
+                style: TextStyle(color: Colors.blue),
+              ),
+            ),
+            // StateNotifierProviderを使ったページ
+            MaterialButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => StateNotifierProviderPage()));
+              },
+              child: Text(
+                'StateNotifierProvider Page',
+                style: TextStyle(color: Colors.blue),
+              ),
+            ),
+            MaterialButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => StreamProviderPage()));
+              },
+              child: Text(
+                'StreamProvider Page',
+                style: TextStyle(color: Colors.blue),
+              ),
+            ),
+            MaterialButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => ChangeNotifierProviderPage()));
+              },
+              child: Text(
+                'Change Notifier Provider Page',
+                style: TextStyle(color: Colors.blue),
+              ),
+            ),
+            MaterialButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => ProductDetailPage()));
+              },
+              child: Text(
+                'Product Detail Page',
+                style: TextStyle(color: Colors.blue),
+              ),
+            ),
+            MaterialButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => ProviderScopePage()));
+              },
+              child: Text(
+                'Provider Scope Page',
+                style: TextStyle(color: Colors.blue),
+              ),
+            ),
+            MaterialButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => FlutterHooksPage()));
+              },
+              child: Text(
+                'Flutter Hooks Page',
+                style: TextStyle(color: Colors.blue),
+              ),
+            ),
+            MaterialButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            SharedPreferencesCacheProviderPage()));
+              },
+              child: Text(
+                'SharedPreferences Cache Provider Page',
+                style: TextStyle(color: Colors.blue),
+              ),
+            ),
+            MaterialButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => ApiResponseFoodPage()));
+              },
+              child: Text(
+                'Api Response Food Page',
+                style: TextStyle(color: Colors.blue),
+              ),
+            ),
+          ],
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-          },
-          tooltip: 'Increment',
-          child: const Icon(Icons.add),
-        ),
-      );
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
